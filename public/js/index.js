3b18908f572e086f0af5741f1aa7e27828158cb0
rashadnm01 <rashadnm01@gmail.com>
2021-10-29 19:21:51 -0500

    Made index.html so I could run live server. Some basic stylinh complete, starting front-end JS for index.liquid

---
+++ public/js/index.js
@@ -0,0 +1,7 @@
+//I need a copy of a row of boxes
+const $row = $("#calendarData");
+const $calendarDiv = $("#calendar");
+$calendarDiv.append($row.clone());
+$calendarDiv.append($row.clone());
+$calendarDiv.append($row.clone());
+$calendarDiv.append($row.clone());
